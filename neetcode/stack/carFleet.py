7b927b5f9a881ecf31a2110fbb8b6e939ac6ffcb
Kaushik <kk12391@gmail.com>
2023-07-25 15:05:11 +0530

    car fleet

---
+++ neetcode/stack/carFleet.py
@@ -0,0 +1,18 @@
+from typing import *
+# https://leetcode.com/problems/car-fleet/
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p, s] for p, s in zip(position, speed)]
+
+        stack = []
+        for p, s in sorted(pair)[::-1]:  # Reverse the sorted order
+            stack.append((target-p)/s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
+
+
+print(Solution().carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]))
